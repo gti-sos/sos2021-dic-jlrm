9110d7cb26b1c8a93a5c85ee6aa1ecbd1633f078
celu99 <jose99luis@hotmail.es>
2021-11-26 23:26:55 +0000

    anadido info/yyyy

--- index.js
+++ index.js
@@ -1,20 +1,24 @@
 var express = require("express");
+var bodyParser = require("body-parser");
+
+var PORT = (process.env.PORT || 10000);
+var BASE_API_PATH = "/api/v1";
 
 var app = express();
 
+app.use(bodyParser.json());
+app.use("/",express.static("./public"));
 
-var port = process.env.PORT || 10000;
 
-app.use("/", express.static("./public"));
 
-app.get("/hello", (req, res) => {
-	res.send("<html><body><h1>GET Hello from this tiny server</h1></body></html>");
-})
+app.get(BASE_API_PATH+"/info/death-stats", (req,res) =>{
+	res.send("<html><body><h1>Analiza la relación que existe entre las causas de muerte a nivel provincial de Andalucía comprendido entre los años 2005 y 2018</h1></body></html>");
+});
 
-app.post("/hello", (req, res) => {
-	res.send("<html><body><h1>POST Hello from this tiny server</h1></body></html>");
-})
+app.post(BASE_API_PATH+"/info/death-stats", (req,res) =>{
+	res.send("<html><body><h1>Analiza la relación que existe entre las causas de muerte a nivel provincial de Andalucía comprendido entre los años 2005 y 2018</h1></body></html>");
+});
 
-app.listen(port,() => {
-	console.log(`DENTRO CALLBACK - Server listening on port ${port}`);
+app.listen(PORT,() => {
+	console.log("Server ready at port " + PORT);
 });
