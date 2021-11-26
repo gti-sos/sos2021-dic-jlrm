c3d08d82042a3c8593477263bd92c7666f6053eb
celu99 <jose99luis@hotmail.es>
2021-11-26 22:34:30 +0000

    Carpeta pubic

--- index.js
+++ index.js
@@ -1,16 +1,20 @@
-var cool = require("cool-ascii-faces");
 var express = require("express");
+
 var app = express();
 
-var port = 10000;
 
-app.get("/cool", (request, response) => {
-	response.send(cool());
-	console.log("New request to /cool has arrived");
-	
-});
+var port = process.env.PORT || 10000;
 
-app.listen(port, () => {
-	console.log("Server ready listening on port " + port);
-	
+app.use("/", express.static("./public"));
+
+app.get("/hello", (req, res) => {
+	res.send("<html><body><h1>GET Hello from this tiny server</h1></body></html>");
+})
+
+app.post("/hello", (req, res) => {
+	res.send("<html><body><h1>POST Hello from this tiny server</h1></body></html>");
+})
+
+app.listen(port,() => {
+	console.log(`DENTRO CALLBACK - Server listening on port ${port}`);
 });
