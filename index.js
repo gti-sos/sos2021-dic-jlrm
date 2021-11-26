3d673db72e5f31fcf4f50019aeea29e308038d33
celu99 <jose99luis@hotmail.es>
2021-11-26 22:11:50 +0000

    terminado cool ascii faces, gitignore y express

---
+++ index.js
@@ -0,0 +1,16 @@
+var cool = require("cool-ascii-faces");
+var express = require("express");
+var app = express();
+
+var port = 10000;
+
+app.get("/cool", (request, response) => {
+	response.send(cool());
+	console.log("New request to /cool has arrived");
+	
+});
+
+app.listen(port, () => {
+	console.log("Server ready listening on port " + port);
+	
+});
