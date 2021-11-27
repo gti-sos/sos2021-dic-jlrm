109843975e13811f89c7518f5edb906cb56fa4a9
celu99 <jose99luis@hotmail.es>
2021-11-27 15:37:37 +0000

    db installed and varibale names fixed

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 var express = require("express");
 var bodyParser = require("body-parser");
+var Datastore = require("nedb");
+
+var db = new Datastore();
 
 var PORT = (process.env.PORT || 10000);
 var BASE_API_PATH = "/api/v1";
@@ -7,46 +10,47 @@ var BASE_API_PATH = "/api/v1";
 var app = express();
 
 app.use(bodyParser.json());
-app.use("/",express.static("./public"));
 
 var deathStats = [
 	{
 		"province" : "almeria",
 		"year" : 2005,
 		"tumor" : 1068,
-		"circulatory-system-disease" : 1454,
-		"respiratory-system-disease" : 573
+		"circulatory_system_disease" : 1454,
+		"respiratory_system_disease" : 573
 	},
 	{
 		"province" : "almeria",
 		"year" : 2006,
 		"tumor" : 1119,
-		"circulatory-system-disease" : 1279,
-		"respiratory-system-disease" : 481
+		"circulatory_system_disease" : 1279,
+		"respiratory_system_disease" : 481
 	},
 		{
 		"province" : "almeria",
 		"year" : 2007,
 		"tumor" : 1134,
-		"circulatory-system-disease" : 1339,
-		"respiratory-system-disease" : 583
+		"circulatory_system_disease" : 1339,
+		"respiratory_system_disease" : 583
 	},
 		{
 		"province" : "almeria",
 		"year" : 2008,
 		"tumor" : 1131,
-		"circulatory-system-disease" : 1375,
-		"respiratory-system-disease" : 573
+		"circulatory_system_disease" : 1375,
+		"respiratory_system_disease" : 573
 	},
 		{
 		"province" : "almeria",
 		"year" : 2009,
 		"tumor" : 1207,
-		"circulatory-system-disease" : 1336,
-		"respiratory-system-disease" : 489
+		"circulatory_system_disease" : 1336,
+		"respiratory_system_disease" : 489
 	}
 	
 ]
+
+db.insert(deathStats);
 
 //Creacion de recursos
 app.get(BASE_API_PATH+"/death-stats/loadInitialData", (req,res) =>{
@@ -54,9 +58,20 @@ app.get(BASE_API_PATH+"/death-stats/loadInitialData", (req,res) =>{
 });
 
 //GET a la lista de recursos
-app.get(BASE_API_PATH+"/death-stats", (req,res) =>{
-	res.send(JSON.stringify(deathStats,null,2));
-});
+app.get(BASE_API_PATH+"/death-stats", (req,res)=>{
+	
+	db.find({}, (err,provincesInDB) => {
+		if(err){
+			console.error("ERROR accessing db in GET" + err);
+			res.sendStatus(500); //Internal server error
+		}else{
+			var provincesToSend = provincesInDB.map((p) => {
+				return {province : p.province, year : p.year, tumor: p.tumor, circulatory_system_disease: p.circulatory_system_disease, respiratory_system_disease: p.respiratory_system_disease}; //Devolver sin el id en postman, _id
+			});
+			res.send(JSON.stringify(provincesToSend,null,2));
+		}
+	});
+ });
 
 //GET a un recurso
 
