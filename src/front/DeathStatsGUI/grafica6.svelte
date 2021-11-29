8fe831266d85aea8c73897acb6536a10a16aea55
celu99 <jose99luis@hotmail.es>
2021-11-29 13:58:57 +0000

    bucle

--- src/front/DeathStatsGUI/grafica6.svelte
+++ src/front/DeathStatsGUI/grafica6.svelte
@@ -65,10 +65,19 @@ Highcharts.chart('container', {
         type: 'pie',
         allowPointSelect: true,
         keys: ['name', 'y', 'selected', 'sliced'],
-        series: [{
-        name: 'Volumen',
-        data: volumen
-}],
+        data: [
+		data_c.forEach((d) => {
+		[d["volume"], d["id"], false],
+        });
+            [nombre, volumen, false],
+            ['Pears', 71.5, false],
+            ['Oranges', 106.4, false],
+            ['Plums', 129.2, false],
+            ['Bananas', 144.0, false],
+            ['Peaches', 176.0, false],
+            ['Prunes', 135.6, true, true],
+            ['Avocados', 148.5, false]
+        ],
         showInLegend: true
     }]
 });
