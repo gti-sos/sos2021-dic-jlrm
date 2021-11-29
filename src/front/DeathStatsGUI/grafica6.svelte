4ea4d563584b5cb4db811e9bbed9e6f063a87059
celu99 <jose99luis@hotmail.es>
2021-11-29 13:43:01 +0000

    fix

--- src/front/DeathStatsGUI/grafica6.svelte
+++ src/front/DeathStatsGUI/grafica6.svelte
@@ -66,7 +66,7 @@ Highcharts.chart('container', {
         allowPointSelect: true,
         keys: ['name', 'y', 'selected', 'sliced'],
         data: [
-            ['Apples', 29.9, false],
+            [nombre, volumen, false],
             ['Pears', 71.5, false],
             ['Oranges', 106.4, false],
             ['Plums', 129.2, false],
