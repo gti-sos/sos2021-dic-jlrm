e47cea3d7be169a68c7dcdd03025e6e5a7934723
celu99 <jose99luis@hotmail.es>
2021-11-29 11:03:51 +0000

    fix 6

--- src/front/DeathStatsGUI/grafica6.svelte
+++ src/front/DeathStatsGUI/grafica6.svelte
@@ -97,6 +97,7 @@ Highcharts.chart('container', {
 
 </script>
 
+<svelte:head>
 <script src="https://code.highcharts.com/highcharts.js"></script>
 <script src="https://code.highcharts.com/highcharts-more.js"></script>
 <script src="https://code.highcharts.com/modules/exporting.js"></script>
