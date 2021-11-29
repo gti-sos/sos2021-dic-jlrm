21a9295f83369058e827d28c37abdebdd9a0546c
celu99 <jose99luis@hotmail.es>
2021-11-29 16:09:22 +0000

    FINAL

--- src/front/About.svelte
+++ src/front/About.svelte
@@ -10,9 +10,9 @@
 
 <br>
 <center>
-<p>El siguiente video muestra una breve explicación del proyecto:</p>
+<p>El siguiente video muestra una breve explicaciรณn del proyecto:</p>
 <br>
 <iframe width="560" height="315" src="https://www.youtube.com/embed/256n8KstXq4" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
 <br>
-<a href="https://youtu.be/256n8KstXq4"<Button>Enlace al video</Button></a></center>
+<a href="https://youtu.be/256n8KstXq4"><Button>Enlace al video</Button></a></center>
 </Container></main>
