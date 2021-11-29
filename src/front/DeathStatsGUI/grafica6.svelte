9a7077fdc996bbc5a3b6d37a9b08830f1dc0276c
celu99 <jose99luis@hotmail.es>
2021-11-29 13:30:14 +0000

    fix

--- src/front/DeathStatsGUI/grafica6.svelte
+++ src/front/DeathStatsGUI/grafica6.svelte
@@ -50,47 +50,42 @@
         });
         
 Highcharts.chart('container', {
+
     chart: {
-        type: 'packedbubble',
-        height: '100%'
+        type: 'column',
+        styledMode: true
     },
+
     title: {
-        text: 'Carbon emissions around the world (2014)'
+        text: 'Styling axes and columns'
     },
-    tooltip: {
-        useHTML: true,
-        pointFormat: '<b>{point.name}:</b> {point.value}m CO<sub>2</sub>'
-    },
+
+    yAxis: [{
+        className: 'highcharts-color-0',
+        title: {
+            text: 'Primary axis'
+        }
+    }, {
+        className: 'highcharts-color-1',
+        opposite: true,
+        title: {
+            text: 'Secondary axis'
+        }
+    }],
+
     plotOptions: {
-        packedbubble: {
-            minSize: '30%',
-            maxSize: '120%',
-            zMin: 0,
-            zMax: 1000,
-            layoutAlgorithm: {
-                splitSeries: false,
-                gravitationalConstant: 0.02
-            },
-            dataLabels: {
-                enabled: true,
-                format: '{point.name}',
-                filter: {
-                    property: 'y',
-                    operator: '>',
-                    value: 250
-                },
-                style: {
-                    color: 'black',
-                    textOutline: 'none',
-                    fontWeight: 'normal'
-                }
-            }
+        column: {
+            borderRadius: 5
         }
     },
+
     series: [{
-        name: 'Europe',
-        data: usd
+        data: btc
+    }, {
+        data: usd,
+        yAxis: 1
     }]
+
 });
 
 }
@@ -98,9 +93,9 @@ Highcharts.chart('container', {
 </script>
 
 <svelte:head>
- <script src="https://code.highcharts.com/highcharts.js"></script>
-    <script src="https://code.highcharts.com/highcharts-more.js"></script>
-    <script src="https://code.highcharts.com/modules/exporting.js"></script>
+<script src="https://code.highcharts.com/highcharts.js"></script>
+<script src="https://code.highcharts.com/modules/exporting.js"></script>
+<script src="https://code.highcharts.com/modules/export-data.js"></script>
 <script src="https://code.highcharts.com/modules/accessibility.js" on:load="{loadGraph}"></script>
 </svelte:head>
 
@@ -129,37 +124,81 @@ Highcharts.chart('container', {
 </main>
 
 <style>
-.highcharts-figure, .highcharts-data-table table {
-    min-width: 320px; 
+@import "https://code.highcharts.com/css/highcharts.css";
+
+.highcharts-figure,
+.highcharts-data-table table {
+    min-width: 310px;
     max-width: 800px;
     margin: 1em auto;
 }
+
 .highcharts-data-table table {
-	font-family: Verdana, sans-serif;
-	border-collapse: collapse;
-	border: 1px solid #EBEBEB;
-	margin: 10px auto;
-	text-align: center;
-	width: 100%;
-	max-width: 500px;
+    font-family: Verdana, sans-serif;
+    border-collapse: collapse;
+    border: 1px solid #ebebeb;
+    margin: 10px auto;
+    text-align: center;
+    width: 100%;
+    max-width: 500px;
 }
+
 .highcharts-data-table caption {
     padding: 1em 0;
     font-size: 1.2em;
     color: #555;
 }
+
 .highcharts-data-table th {
-	font-weight: 600;
+    font-weight: 600;
     padding: 0.5em;
 }
-.highcharts-data-table td, .highcharts-data-table th, .highcharts-data-table caption {
+
+.highcharts-data-table td,
+.highcharts-data-table th,
+.highcharts-data-table caption {
     padding: 0.5em;
 }
-.highcharts-data-table thead tr, .highcharts-data-table tr:nth-child(even) {
+
+.highcharts-data-table thead tr,
+.highcharts-data-table tr:nth-child(even) {
     background: #f8f8f8;
 }
+
 .highcharts-data-table tr:hover {
     background: #f1f7ff;
 }
+
+.highcharts-yaxis .highcharts-axis-line {
+    stroke-width: 2px;
+}
+
+/* Link the series colors to axis colors */
+.highcharts-color-0 {
+    fill: #7cb5ec;
+    stroke: #7cb5ec;
+}
+
+.highcharts-axis.highcharts-color-0 .highcharts-axis-line {
+    stroke: #7cb5ec;
+}
+
+.highcharts-axis.highcharts-color-0 text {
+    fill: #7cb5ec;
+}
+
+.highcharts-color-1 {
+    fill: #90ed7d;
+    stroke: #90ed7d;
+}
+
+.highcharts-axis.highcharts-color-1 .highcharts-axis-line {
+    stroke: #90ed7d;
+}
+
+.highcharts-axis.highcharts-color-1 text {
+    fill: #90ed7d;
+}
+
 
 </style>
