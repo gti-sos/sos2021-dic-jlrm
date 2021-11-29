333c68f9c66ba5028acc8f8d61a7b767857ee084
celu99 <jose99luis@hotmail.es>
2021-11-29 12:31:07 +0000

    fix scripts

--- src/front/DeathStatsGUI/grafica6.svelte
+++ src/front/DeathStatsGUI/grafica6.svelte
@@ -99,9 +99,9 @@
 </script>
 
 <svelte:head>
-<script src="https://code.highcharts.com/highcharts.js"></script>
-<script src="https://code.highcharts.com/highcharts-3d.js"></script><script src="https://code.highcharts.com/modules/exporting.js"></script>
-<script src="https://code.highcharts.com/modules/export-data.js"></script>
+ <script src="https://code.highcharts.com/highcharts.js"></script>
+    <script src="https://code.highcharts.com/highcharts-more.js"></script>
+    <script src="https://code.highcharts.com/modules/exporting.js"></script>
 <script src="https://code.highcharts.com/modules/accessibility.js" on:load="{loadGraph}"></script>
 </svelte:head>
 
