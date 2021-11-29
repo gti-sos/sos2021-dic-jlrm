d5d29e4e1e9deb3770d5e13270d44b6b2f2c1d38
celu99 <jose99luis@hotmail.es>
2021-11-29 12:57:08 +0000

    fin diseno

--- src/front/Integrations.svelte
+++ src/front/Integrations.svelte
@@ -9,15 +9,16 @@
 <Container style="background-color:#121212; color:white; border-radius:4px; padding: 20px">
 
 <br>
+Este proyecto tiene implementadas las siguientes tablas o graficas:
 <center>
 <ButtonGroup>
-  <a href="/#/death-stats"><Button active>Tabla de la API</Button></a>
-  <a href="/#/death-stats-graph1"><Button>Grafico de la API</Button></a>
-  <a href="#/graph3"><Button>Grafico integrado</Button></a>
-  <a href="#/graph2"><Button>Grafico de SOS 1</Button></a>
-  <a href="#/graph4"><Button>Grafico de SOS 2</Button></a>
-  <a href="#/graph5"><Button>Grafico externo 1</Button></a>
-  <a href="#/graph6"><Button>Grafico externo 2</Button></a>
+  <a href="/#/death-stats"><Button light style="margin:2px;">Tabla de la API</Button></a>
+  <a href="/#/death-stats-graph1"><Button light style="margin:2px;>Grafico de la API</Button></a>
+  <a href="#/graph3"><Button light style="margin:2px;>Grafico integrado</Button></a>
+  <a href="#/graph2"><Button light style="margin:2px;>Grafico de SOS 1</Button></a>
+  <a href="#/graph4"><Button light style="margin:2px;>Grafico de SOS 2</Button></a>
+  <a href="#/graph5"><Button light style="margin:2px;>Grafico externo 1</Button></a>
+  <a href="#/graph6"><Button light style="margin:2px;>Grafico externo 2</Button></a>
 
 </ButtonGroup>
 </center>
