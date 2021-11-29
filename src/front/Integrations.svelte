2b5d4e24e18fa3c8c755d199dd06f4a6a72f0727
celu99 <jose99luis@hotmail.es>
2021-11-29 13:04:37 +0000

    fix design

--- src/front/Integrations.svelte
+++ src/front/Integrations.svelte
@@ -12,13 +12,13 @@
 Este proyecto tiene implementadas las siguientes tablas o graficas:
 <center>
 <ButtonGroup>
-  <a href="/#/death-stats"><Button style="margin:2px;">Tabla de la API</Button></a>
-  <a href="/#/death-stats-graph1"><Button style="margin:2px;>Grafico de la API</Button></a>
-  <a href="#/graph3"><Button style="margin:2px;>Grafico integrado</Button></a>
-  <a href="#/graph2"><Button style="margin:2px;>Grafico de SOS 1</Button></a>
-  <a href="#/graph4"><Button style="margin:2px;>Grafico de SOS 2</Button></a>
-  <a href="#/graph5"><Button style="margin:2px;>Grafico externo 1</Button></a>
-  <a href="#/graph6"><Button style="margin:2px;>Grafico externo 2</Button></a>
+  <a href="/#/death-stats"><Button light style="margin:2px;">Tabla de la API</Button></a>
+  <a href="/#/death-stats-graph1"><Button light style="margin:2px;">Grafico de la API</Button></a>
+  <a href="#/graph3"><Button light style="margin:2px;">Grafico integrado</Button></a>
+  <a href="#/graph2"><Button light style="margin:2px;">Grafico de SOS 1</Button></a>
+  <a href="#/graph4"><Button light style="margin:2px;">Grafico de SOS 2</Button></a>
+  <a href="#/graph5"><Button light style="margin:2px;">Grafico externo 1</Button></a>
+  <a href="#/graph6"><Button light style="margin:2px;">Grafico externo 2</Button></a>
 
 </ButtonGroup>
 </center>
--- src/front/Landing.svelte
+++ src/front/Landing.svelte
@@ -1,6 +1,6 @@
 <script>
  import {
-	Col, Container, Row
+	Col, Container, Row, Button, ButtonGroup
   } from 'sveltestrap';
   
 </script>
@@ -10,6 +10,17 @@
 <Container style="background-color:#121212; color:white; border-radius:4px; padding: 20px">
 	<center><img src="./images/mapa.png" /></center>
   <Row>
-    <Col>.col</Col>
+    <Col><center>
+<ButtonGroup>
+  <a href="/#/death-stats"><Button light style="margin:2px;">Tabla de la API</Button></a>
+  <a href="/#/death-stats-graph1"><Button light style="margin:2px;">Grafico de la API</Button></a>
+  <a href="#/graph3"><Button light style="margin:2px;">Grafico integrado</Button></a>
+  <a href="#/graph2"><Button light style="margin:2px;">Grafico de SOS 1</Button></a>
+  <a href="#/graph4"><Button light style="margin:2px;">Grafico de SOS 2</Button></a>
+  <a href="#/graph5"><Button light style="margin:2px;">Grafico externo 1</Button></a>
+  <a href="#/graph6"><Button light style="margin:2px;">Grafico externo 2</Button></a>
+
+</ButtonGroup>
+</center></Col>
   </Row>
 </Container></main>
